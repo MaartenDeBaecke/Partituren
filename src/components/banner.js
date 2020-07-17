fd4b5229022ecbbb3a17782c20800e23d0411ca7
maartendebaecke <maartendebaecke@gmail.com>
2020-07-17 16:48:43 +0200

    first version banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Search from './search';
 
 
 function Banner() {
@@ -6,9 +7,7 @@ function Banner() {
     <div className="banner">
       <h1 className="nameTitle">Steven De Baecke</h1>
       <p className="nameSubTitle">partituren</p>
-      <form className="searchForm">
-        <input className="search" type="text" name="search" autoComplete="off" placeholder="zoeken" />
-      </form>
+      <Search />
       <div className="btn">
         <span className="noSelect">Overzicht</span><div id="circle">
       </div></div>
