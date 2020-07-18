8f6026463a6d6852dabcbc9675de2be43ba8bd48
maartendebaecke <maartendebaecke@gmail.com>
2020-07-18 10:54:45 +0200

    structured components banner

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return(
+    <div>
+      <h1 className="nameTitle">Steven De Baecke</h1>
+      <p className="nameSubTitle">partituren</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Overzicht.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Overzicht(){
+  return(
+    <div className="btn">
+      <span className="noSelect">Overzicht</span><div id="circle"></div>
+    </div>
+  );
+}
+
+export default Overzicht;
