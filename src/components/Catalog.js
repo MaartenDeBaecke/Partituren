07558d58070ac9cd0c7073a040db9626d02f1718
maartendebaecke <maartendebaecke@gmail.com>
2020-07-18 14:48:29 +0200

    first take on design card

---
+++ src/components/Catalog.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Element from "./Card";
+
+function Catalog(){
+  return(
+    <div className="catalog">
+      <hr />
+      <h1>Nieuw</h1>
+      <Element />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    </div>
+  );
+}
+
+export default Catalog;
