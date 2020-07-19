21e0c073cefa28ad5a8cc06509fd23c07543c77d
maartendebaecke <maartendebaecke@gmail.com>
2020-07-19 14:41:49 +0200

    Make buttons interactive

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,9 +16,12 @@ function Element(){
             <p>Gepubliceerd in 2003</p>
           </div>
           <div className="desbtn">
-            <button className="buybtn"><i class="fas fa-file-import icon"></i>Koop nu</button>
 
-            <button className="playbtn">&nbsp;<i class="fas fa-play" ></i></button>
+            <span className="shine"><button className="buybtn"><i class="fas fa-file-import icon"></i>Koop nu</button></span>
+
+
+            <button className="playbtn">&nbsp;<a className="playLink" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=9a0KyR_l2tw"><i class="fas fa-play" ></i></a></button>
+
           </div>
         </div>
       </Jumbotron>
