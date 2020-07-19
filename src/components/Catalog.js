bf879af4260978549367358124c3934cc20021d4
maartendebaecke <maartendebaecke@gmail.com>
2020-07-19 16:02:18 +0200

    scalled cards down

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,26 +1,23 @@
 import React from "react";
 import Element from "./card/Card";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Catalog(){
   return(
     <div className="catalog">
       <hr />
       <h1>Nieuw</h1>
-      <Element />
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+
+      <Container className="catalogContainer">
+        <Row class="row">
+          <Col lg={4} md={6}><Element /></Col>
+          <Col lg={4} md={6}><Element /></Col>
+          <Col lg={4} md={6}><Element /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
