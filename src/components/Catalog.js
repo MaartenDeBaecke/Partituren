a5dd48691e73f147a5a20e52695fb0be435fd99c
maartendebaecke <maartendebaecke@gmail.com>
2020-07-21 14:17:24 +0200

    scalling size of cards down and fixing grid problem

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -8,20 +8,21 @@ import Col from 'react-bootstrap/Col';
 function Catalog(){
   return(
     <div className="catalog">
-      <hr />
       <h1>Nieuw</h1>
 
       <Container className="catalogContainer">
         <Row className="row">
-          <Col className="col-lg-4"><Card title="Morning Blue" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/16/morning_blue.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="4 Mysterious Pieces" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/26/four_mysterious_pieces.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="Luister en Speel" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/15/luister_en_speel_begeleiding.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="Peter Pan" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/24/2_pieces_for_double_bass.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="afscheid" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/13/Peter_Pan.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="Fingers crossed" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/3/Afscheid.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="6 Dynamic Pieces" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/27/fingers_crossed.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="Luister en Speel" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/22/6_Dynamic_pieces.jpg"/></Col>
-          <Col className="col-lg-4"><Card title="Met de fluit" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/11/spelenderwijs_met_de_fluit.jpg"/></Col>
+          <Col className="col"><Card title="Morning Blue" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/16/morning_blue.jpg"/></Col>
+          <Col className="col"><Card title="4 Mysterious Pieces" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/26/four_mysterious_pieces.jpg"/></Col>
+          <Col className="col"><Card title="Luister en Speel" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/15/luister_en_speel_begeleiding.jpg"/></Col>
+
+          <Col className="col"><Card title="Peter Pan" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/24/2_pieces_for_double_bass.jpg"/></Col>
+          <Col className="col"><Card title="afscheid" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/13/Peter_Pan.jpg"/></Col>
+          <Col className="col"><Card title="Fingers crossed" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/3/Afscheid.jpg"/></Col>
+
+          <Col className="col"><Card title="6 Dynamic Pieces" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/27/fingers_crossed.jpg"/></Col>
+          <Col className="col"><Card title="Luister en Speel" link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/22/6_Dynamic_pieces.jpg"/></Col>
+          <Col className="col"><Card title="Luister en Speel"link="https://sdb-steven-de-baecke.s3.eu-west-2.amazonaws.com/uploads/my_music/image/11/spelenderwijs_met_de_fluit.jpg"/></Col>
         </Row>
       </Container>
     </div>
