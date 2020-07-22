f1f3d11ded387637172830b98a0fce5e341b2d9e
Maarten De Baecke <40885625+MaartenDeBaecke@users.noreply.github.com>
2020-07-22 15:39:25 +0200

    Revert "Catalogdesign"

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import Banner from './banner';
 import Catalog from './Catalog';
 
-
 function App() {
   return(
     <div>
-      <section id="banner">
-        <Banner />
-      </section>
-      <section id="overzicht">
-        <Catalog />
-      </section>
+      <Banner />
+      <Catalog />
     </div>
   );
-}
+} 
 
 export default App;
--- src/components/card/img.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Img(props){
-  console.log(props.url);
-  return(<img id="cardImg" src={props.url} alt="dwarsfluit" />);
-}
-
-export default Img;
