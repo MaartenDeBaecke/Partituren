febf9d14d913b44d126e23117efee13284e69660
maartendebaecke <maartendebaecke@gmail.com>
2020-08-12 15:32:17 +0200

    subbanner

--- src/components/SubBanner.js
+++ src/components/SubBanner.js
@@ -14,34 +14,34 @@ function SubBanner(){
 
   return(
     <div className="subBanCont">
-      <Row>
-      <Col className="highDes" xl={6}>
-        <h2 className="subBanTitle">Verkrijg een gratis kopie</h2>
-        <p className="subBanDes">
-          Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco
-        </p>
-        <div className="desbtn subBanBtn">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={buyUrl}>
-            <span className="shine">
-              <button className="buybtn" >
-                <i className="fas fa-file-import icon"></i>Gratis
-              </button>
-            </span>
-          </a>
-        </div>
-      </Col>
-      <Col xl={6}>
-        <Highlight />
-      </Col>
+      <Row className="subBanRow">
+        <Col className="highDes" lg={6}>
+          <h2 className="subBanTitle">Verkrijg een gratis kopie</h2>
+          <p className="subBanDes">
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco
+          </p>
+        </Col>
+        <Col className="highCard" lg={6}>
+          <Highlight />
+        </Col>
       </Row>
+      <div className="desbtn subBanBtn">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={buyUrl}>
+          <span className="shine">
+            <button className="buybtn" >
+              <i className="fas fa-file-import icon"></i>Gratis
+            </button>
+          </span>
+        </a>
+      </div>
     </div>
   );
 }
