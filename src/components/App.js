1038137064b5e5c4eff717a820c77fca72c04ebf
maartendebaecke <maartendebaecke@gmail.com>
2020-08-18 17:09:34 +0200

    styled registreer and aanmelden page

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import EditCard from "./EditCard";
 import CreateCard from "./CreateCard";
-import Register from "./Register";
-import Login from "./Login";
 
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/edit/:id" component={EditCard} />
       <Route path="/create" component={CreateCard} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
     </Router>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,18 +3,25 @@ import PopUp from "./NavPopUp";
 
 function Nav(){
   const [clicked, setClicked] = useState(false);
+  const [name, setName] = useState("");
 
-  function clicking(){
+  function clicking1(){
     setClicked(true);
+    setName("Registreren");
+  }
+
+  function clicking2(){
+    setClicked(true);
+    setName("Aanmelden");
   }
 
   return(
     <div>
-      {clicked ? <PopUp popUp={value => setClicked(value)} /> : null }
+      {clicked ? <PopUp name={name} popUp={value => setClicked(value)} /> : null }
 
       <div className="navCont">
-        <button className="navEl" onClick={clicking}>Registreren</button>
-        <button className="navEl" >Aanmelden</button>
+        <button className="navEl" onClick={clicking1}>Registreren</button>
+        <button className="navEl" onClick={clicking2} >Aanmelden</button>
       </div>
     </div>
   );
